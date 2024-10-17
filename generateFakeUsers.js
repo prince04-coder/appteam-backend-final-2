b173784c9fe5a4b90db2cc78f11cd550ffd6d415
prince04-coder <23bma031@nith.ac.in>
2024-10-17 22:37:18 +0530

    matchmaking done and voting schema ommited

--- generateFakeUsers.js
+++ generateFakeUsers.js
@@ -1,46 +1,6 @@
 
 
 
-// const mongoose = require('mongoose');
-// const { faker } = require('@faker-js/faker'); // Import faker from @faker-js/faker
-// const User = require('./models/User'); // Update with the correct path to your User model
-
-// // Connect to MongoDB
-// const mongoURI = 'mongodb+srv://khandelwalprince04:appteam@cluster0.hsu22.mongodb.net/hillfair'; // Replace with your MongoDB connection string
-
-// mongoose.connect(mongoURI, { useUnifiedTopology: true }) // Removed useNewUrlParser
-//   .then(() => {
-//     console.log('MongoDB connected successfully.');
-//     generateFakeUsers(100); // Specify the number of fake users to create
-//   })
-//   .catch((err) => console.error('MongoDB connection error:', err));
-
-// // Function to generate fake users
-// async function generateFakeUsers(numUsers) {
-//   try {
-//     const users = [];
-    
-//     for (let i = 0; i < numUsers; i++) {
-//       const user = {
-//         username: faker.internet.userName(),
-//         quizAnswers: Array.from({ length: 5 }, () => faker.number.int({ min: 1, max: 10 })), // Correct method to generate random number
-//         gender: faker.helpers.arrayElement(['male', 'female']), // Randomly select gender
-//       };
-//       users.push(user);
-//     }
-    
-//     // Insert fake users into the database
-//     await User.insertMany(users);
-//     console.log(`${numUsers} fake users generated successfully.`);
-//   } catch (error) {
-//     console.error('Error generating fake users:', error);
-//   } finally {
-//     mongoose.connection.close(); // Close the connection
-//   }
-// }
-
-
-
 
 
 
