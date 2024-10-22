b0b195722492d86bdcca4c8f6d1aee4001d06acb
Sankalp Gupta <120783585+sankalp2102@users.noreply.github.com>
2024-10-22 11:05:10 +0530

    Update chatRoutes.js

--- routes/chatRoutes.js
+++ routes/chatRoutes.js
@@ -6,11 +6,11 @@ const { sendMessage, joinRoomAndFetchMessages } = require('../controllers/chat/c
  * Route for joining a room and fetching previous messages
  */
 router.post('/join-room', (req, res) => {
-    const { userId1, userId2 } = req.body;
-    req.socket.emit('joinRoom', userId1, userId2);
-    res.status(200).json({ message: 'Joined room and fetched previous messages' });
+    //const { userId1, userId2 } = req.body;
+    //req.socket.emit('joinRoom', userId1, userId2);
+    //res.status(200).json({ message: 'Joined room and fetched previous messages' });
+    joinRoomAndFetchMessages
 });
-
 // POST route to send a message
 router.post('/send', (req, res) => {
   const { userId1, userId2, messageContent } = req.body;
