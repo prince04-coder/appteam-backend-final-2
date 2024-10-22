6cf54d1492b6135ea31ed683358871b7f51bbe98
prince04-coder <23bma031@nith.ac.in>
2024-10-22 10:53:43 +0530

    fix join chat room api

--- routes/chatRoutes.js
+++ routes/chatRoutes.js
@@ -5,11 +5,14 @@ const { sendMessage, joinRoomAndFetchMessages } = require('../controllers/chat/c
 /**
  * Route for joining a room and fetching previous messages
  */
-router.post('/join-room', (req, res) => {
-    const { userId1, userId2 } = req.body;
-    req.socket.emit('joinRoom', userId1, userId2);
-    res.status(200).json({ message: 'Joined room and fetched previous messages' });
-});
+router.post('/join-room', 
+//   (req, res) => {
+//     const { userId1, userId2 } = req.body;
+//     req.socket.emit('joinRoom', userId1, userId2);
+//     res.status(200).json({ message: 'Joined room and fetched previous messages' });
+// }
+joinRoomAndFetchMessages
+);
 
 // POST route to send a message
 router.post('/send', (req, res) => {
